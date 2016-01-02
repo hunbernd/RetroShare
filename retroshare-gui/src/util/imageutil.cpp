491365885727b8a8431207139b24d5f5138bd284
hunbernd <hunbernd@outlook.com>
2016-01-02 02:46:17 +0100

    Downscale images automatically

--- retroshare-gui/src/util/imageutil.cpp
+++ retroshare-gui/src/util/imageutil.cpp
@@ -8,6 +8,10 @@
 #include <QImage>
 #include <QTextDocumentFragment>
 #include <QApplication>
+#include <QBuffer>
+#include <QtGlobal>
+#include <QtMath>
+#include <iostream>
 
 ImageUtil::ImageUtil() {}
 
@@ -69,4 +73,94 @@ void ImageUtil::cropBorders(QImage &img)
 		img = QImage();
 	else
 		img = img.copy(l, t, r-l+1, b-t+1);
+}
+
+void ImageUtil::optimizeSize(QString &html, const QImage& original, QImage &optimized, int maxPixels, int maxBytes)
+{
+	optimized = original;
+	if ((maxPixels <= 0) || (optimized.width()*optimized.height() <= maxPixels)) {
+		if(checkSize(html, optimized, maxBytes)) {
+			return;
+		}
+	}
+
+	cropBorders(optimized);
+	if (optimized.width()*optimized.height() <= maxPixels) {
+		if(checkSize(html, optimized, maxBytes)) {
+			return;
+		}
+	}
+
+	QImage cropped = optimized;
+
+	//Downscale the image to fit into maxPixels
+	qreal scale = qSqrt((qreal)(maxPixels) / cropped.width() / cropped.height());
+	if(scale > 1.0) scale = 1.0;
+
+	//Check with max quality
+	optimized = cropped.scaledToWidth((int)(cropped.width() * scale), Qt::SmoothTransformation);
+	if(checkSize(html, optimized, maxBytes)) {
+		return;
+	}
+
+	//Set quality to 0 and half the resolution until image fits into maxBytes, or width becomes 0
+	do {
+		optimized = cropped.scaledToWidth((int)(cropped.width() * scale), Qt::SmoothTransformation);
+		scale = scale / 2.0;
+	} while((!optimized.isNull()) && (!checkSize(html, optimized, maxBytes, 0)));
+}
+
+bool ImageUtil::checkSize(QString &embeddedImage, const QImage &img, int maxBytes, int quality)
+{
+	QByteArray bytearray;
+	QBuffer buffer(&bytearray);
+
+	std::cout << QString("Trying image: format PNG, size %1x%2, quality %3\n").arg(img.width()).arg(img.height()).arg(quality).toStdString();
+	if (buffer.open(QIODevice::WriteOnly)) {
+		if (img.save(&buffer, "PNG", quality)) {
+			QByteArray encodedByteArray = bytearray.toBase64();
+			embeddedImage = "<img src=\"data:image/png;base64,";
+			embeddedImage.append(encodedByteArray);
+			embeddedImage.append("\">");
+			if((maxBytes > 0) && (embeddedImage.size() > maxBytes))
+			{
+				std::cout << QString("\tToo large, size: %1, limit: %2 bytes\n").arg(embeddedImage.size()).arg(maxBytes).toStdString();
+			}else{
+				std::cout << QString("\tOK, size: %1, limit: %2 bytes\n").arg(embeddedImage.size()).arg(maxBytes).toStdString();
+				return true;
+			}
+		} else {
+			std::cerr << "ImageUtil: image can't be saved to buffer" << std::endl;
+		}
+		buffer.close();
+		bytearray.clear();
+	} else {
+		std::cerr << "ImageUtil: buffer can't be opened" << std::endl;
+	}
+
+	std::cout << QString("Trying image: format JPEG, size %1x%2, quality %3\n").arg(img.width()).arg(img.height()).arg(quality).toStdString();
+	if (buffer.open(QIODevice::WriteOnly)) {
+		if (img.save(&buffer, "JPEG", quality)) {
+			QByteArray encodedByteArray = bytearray.toBase64();
+			embeddedImage = "<img src=\"data:image/jpeg;base64,";
+			embeddedImage.append(encodedByteArray);
+			embeddedImage.append("\">");
+			if((maxBytes > 0) && (embeddedImage.size() > maxBytes))
+			{
+				std::cout << QString("\tToo large, size: %1, limit: %2 bytes\n").arg(embeddedImage.size()).arg(maxBytes).toStdString();
+			}else{
+				std::cout << QString("\tOK, size: %1, limit: %2 bytes\n").arg(embeddedImage.size()).arg(maxBytes).toStdString();
+				return true;
+			}
+		} else {
+			std::cerr << "ImageUtil: image can't be saved to buffer" << std::endl;
+		}
+		buffer.close();
+		bytearray.clear();
+	} else {
+		std::cerr << "ImageUtil: buffer can't be opened" << std::endl;
+	}
+
+	embeddedImage.clear();
+	return false;
 }
--- retroshare-gui/src/util/imageutil.h
+++ retroshare-gui/src/util/imageutil.h
@@ -4,6 +4,7 @@
 #include <QTextCursor>
 #include <QWidget>
 #include <QImage>
+#include <QString>
 
 
 class ImageUtil
@@ -15,6 +16,10 @@ public:
 	static bool checkImage(QTextCursor cursor);
 
 	static void cropBorders(QImage& img);
+	static void optimizeSize(QString &html, const QImage& original, QImage &optimized, int maxPixels = -1, int maxBytes = -1);
+
+private:
+	static bool checkSize(QString& embeddedImage, const QImage& img, int maxBytes = -1, int quality = -1);
 };
 
 #endif // IMAGEUTIL_H
